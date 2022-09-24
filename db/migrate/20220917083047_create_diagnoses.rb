a77843aa8a316aee08b8a8a6cd6607e68ac80383
subaru-hello <octosubaru0926@gmail.com>
2022-09-24 21:51:13 +0900

    add user relation to diagnoses

--- db/migrate/20220917083047_create_diagnoses.rb
+++ db/migrate/20220917083047_create_diagnoses.rb
@@ -3,6 +3,8 @@
 class CreateDiagnoses < ActiveRecord::Migration[6.1]
   def change
     create_table :diagnoses do |t|
+      t.references :user
+      t.integer :weight, null: false
       t.integer :alcohol_tolerance, default: 0
       t.integer :recommended_liquor, default: 0
       t.string :alcohol_index, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,38 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_917_085_729) do
-  create_table 'alcohols', force: :cascade do |t|
-    t.string 'name', null: false
-    t.integer 'price', null: false
-    t.string 'percentage', null: false
-    t.integer 'amount', null: false
-    t.date 'expiration_date', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2022_09_17_085729) do
+
+  create_table "alcohols", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.string "percentage", null: false
+    t.integer "amount", null: false
+    t.date "expiration_date", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'diagnoses', force: :cascade do |t|
-    t.integer 'alcohol_tolerance', default: 0
-    t.integer 'recommended_liquor', default: 0
-    t.string 'alcohol_index', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "diagnoses", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "weight", null: false
+    t.integer "alcohol_tolerance", default: 0
+    t.integer "recommended_liquor", default: 0
+    t.string "alcohol_index", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diagnoses_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'uuid', null: false
-    t.string 'name', null: false
-    t.integer 'liver_capacity', default: 0
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-    t.index ['uuid'], name: 'index_users_on_uuid'
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "uuid", null: false
+    t.string "name", null: false
+    t.integer "liver_capacity", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uuid"], name: "index_users_on_uuid"
   end
+
 end
