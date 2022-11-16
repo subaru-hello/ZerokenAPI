1c4b501a6b5ddf31c7ba7db6c6d745346c53f420
subaru-hello <octosubaru0926@gmail.com>
2022-11-16 20:59:55 +0900

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :analyses, dependent: :destroy
+  has_many :diagnoses, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   before_create -> { self.uuid = SecureRandom.uuid }
