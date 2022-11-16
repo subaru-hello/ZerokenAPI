7f18a1e8e02d531f7f3ea229d0b9d03d0905a7e3
subaru-hello <octosubaru0926@gmail.com>
2022-11-16 22:37:03 +0900

    factories

---
+++ spec/factories/analyses.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :analysis do
+    answer_1 { 1 }
+    answer_2 { 1 }
+    answer_3 { 1 }
+    answer_4 { 1 }
+    answer_5 { 1 }
+    answer_6 { 1 }
+    answer_7 { 1 }
+    answer_8 { 1 }
+    answer_9 { 1 }
+    answer_10 { 1 }
+    answer_11 { 1 }
+    answer_12 { 1 }
+    answer_13 { 1 }
+  end
+end
