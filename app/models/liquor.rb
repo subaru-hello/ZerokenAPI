d3d107030144e6a96a7dad82f84cb5e301b03d47
subaru-hello <octosubaru0926@gmail.com>
2022-11-17 20:08:01 +0900

    change assosiation order

--- app/models/liquor.rb
+++ app/models/liquor.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Liquor < ApplicationRecord
-  has_many :categories, through: :category_tags
   has_many :category_tags, dependent: :destroy
+  has_many :categories, through: :category_tags
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :analyses, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :liquors, through: :favorites
   has_many :diagnoses, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
